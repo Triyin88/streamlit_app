d825eeec523339a3dfa745b2b2e39d4a3bcdf8ff
Triyin88 <fongyitan@gmail.com>
2024-05-14 20:51:13 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title("Hello World")
+
+import pandas as pd
+import numpy as np
+import time
+
+animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+
+animal = st.text_input('Type an animal')
+
+if st.button('Check availability'):
+    have_it = animal.lower() in animal_shelter
+    'We have that animal!' if have_it else 'We don\'t have that animal.'
